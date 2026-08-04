efe99dde8b036fd27e74e00cbdf0f3583bd0acf9
anshhhiiid <anshiinew@gmail.com>
2026-08-04 09:51:51 +0530

    Time: 0 ms (100%), Space: 53.3 MB (76.16%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    return s.toLowerCase();
+  
+    
+};
+console.log(toLowerCase("Hello"));
+console.log(toLowerCase("here"));
+console.log(toLowerCase("LOVELY"));
