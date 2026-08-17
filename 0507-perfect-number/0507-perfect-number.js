b666df0b64a3e9a569869086910931ce727b75bc
anshhhiiid <anshiinew@gmail.com>
2026-08-17 21:25:47 +0530

    Time: 2110 ms (5.24%), Space: 55 MB (48.85%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function(num) {
+    let sum =0;
+    for(let i=1;i<num ; i++){
+        if (num % i ===0){
+            sum+=i;
+        }
+    }
+    return sum === num;
+}
+console.log(checkPerfectNumber(28));
+console.log(checkPerfectNumber(7));
