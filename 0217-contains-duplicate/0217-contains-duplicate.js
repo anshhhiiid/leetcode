54ed003fb7c69437c655afeb531195ae464d4c67
anshhhiiid <anshiinew@gmail.com>
2026-09-04 10:10:16 +0530

    Time: 25 ms (53.21%), Space: 78.5 MB (80.75%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    const s =new Set(nums);
+    return s.size !== nums.length
+};
+console.log(containsDuplicate([1,2,3,5,7,1]));
